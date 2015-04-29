2a00abf01bcbadd9d3e85abbc64fc41fa6ec5a83
Yong <Yong@android-20013fea6bcc8330c.local>
2015-04-28 20:32:06 -0400

    quick python starter code

---
+++ sketchpad.py
@@ -0,0 +1,19 @@
+import unittest, math, sys
+
+
+class TestMyTest(unittest.TestCase):
+
+    def testTest1(self):
+        return
+
+
+#def readAllFromStdin():
+#    lines = []
+#    for line in sys.stdin:
+#        lines.append(line)
+#    return revertSentence("".join(lines))
+
+
+
+if __name__ == '__main__':
+    unittest.main()
